811d1e097ecbd2c3de804c0a0dc269d529195b60
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-10-20 23:33:04 -0300

    Implementa atualização de datetime dos peers no ping

--- Server/Controllers/NetsphereController.cs
+++ Server/Controllers/NetsphereController.cs
@@ -10,8 +10,9 @@ namespace Netsphere.Controllers
     [Route("[controller]")]
     public class Netsphere : ControllerBase
     {
+        private static object Locker = new object();
         private static HashSet<Peer> Peers = new HashSet<Peer>();
-        private static Timer Timer = new Timer(4000);
+        private static Timer Timer = new Timer(5000);
 
         static Netsphere()
         {
@@ -22,29 +23,41 @@ namespace Netsphere.Controllers
 
         private static void OnRemoveInvalidPeers(object source, ElapsedEventArgs e)
         {
-            lock(Peers)
+            lock(Locker)
             {
                 var currentTime = DateTime.Now;                
-                Peers.RemoveWhere(p => (p.Timestamp- currentTime).TotalSeconds > 5);
+                Peers.RemoveWhere(p => Math.Abs((currentTime - p.Timestamp).Seconds) > 5);
             }
         }
 
-        [HttpPost("Ping")]
-        public ActionResult<string> Ping() => Ok("Pong");
+        [HttpPost("Ping/{ipEndPoint}")]
+        public ActionResult<string> Ping(string ipEndPoint)
+        {
+            lock(Locker)
+            {
+                var peer = Peers.Where(p => p.IPEndPoint == ipEndPoint).FirstOrDefault();
+                if(peer != null)
+                {
+                    peer.Timestamp = DateTime.Now;
+                }
 
-        [HttpPost("Register")]
+                return Ok("Pong");
+            }
+        }
+
+        [HttpPost("Register/{peer}")]
         public ActionResult Register(Peer peer)
         {
-            lock(Peers)
+            lock(Locker)
             {
-                return Peers.Add(peer) ? Ok() : Forbid() as ActionResult;
+                return Peers.Add(peer) ? Ok() : BadRequest() as ActionResult;
             }
         }
 
         [HttpGet("QueryCatalog")]
         public ActionResult<List<string>> QueryCatalog()
         {
-            lock(Peers)
+            lock(Locker)
             {
                 return Ok(Peers.SelectMany(p => p.AvailableContent));
             }
