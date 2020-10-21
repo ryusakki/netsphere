ec7f2f8528f379fe9b3f49cc973bfba0caafe383
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-10-20 21:28:36 -0300

    Vai cavalo

---
+++ Server/Controllers/NetsphereController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Timers;
+using System.Linq;
+
+namespace Netsphere.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Netsphere : ControllerBase
+    {
+        private static HashSet<Peer> Peers = new HashSet<Peer>();
+        private static Timer Timer = new Timer(4000);
+
+        static Netsphere()
+        {
+            Timer.Elapsed += OnRemoveInvalidPeers;
+            Timer.AutoReset = true;
+            Timer.Enabled = true;
+        }
+
+        private static void OnRemoveInvalidPeers(object source, ElapsedEventArgs e)
+        {
+            lock(Peers)
+            {
+                var currentTime = DateTime.Now;                
+                Peers.RemoveWhere(p => (currentTime - p.Timestamp).TotalSeconds > 5);
+            }
+        }
+
+        [HttpPost("Ping")]
+        public ActionResult<string> Ping() => Ok("Pong");
+
+        [HttpPost("Register")]
+        public ActionResult Register(Peer peer)
+        {
+            lock(Peers)
+            {
+                return Peers.Add(peer) ? Ok() : Forbid() as ActionResult;
+            }
+        }
+
+        [HttpGet("QueryCatalog")]
+        public ActionResult<List<string>> QueryCatalog()
+        {
+            lock(Peers)
+            {
+                return Ok(Peers.SelectMany(p => p.AvailableContent));
+            }
+        }
+    }
+}
