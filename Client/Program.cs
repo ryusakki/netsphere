4c759708377f617dd68d0877e5c7a13ea813d925
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-10-28 20:24:24 -0300

    Entry Point

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,23 +1,60 @@
 ﻿using Client;
+using Netsphere.Client.Enums;
+using Netsphere.Client.Extensions;
+using Netsphere.Client.UI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Netsphere.Client
 {
     public class Program
     {
+        private static UIContext Interface = new UIContext("Netsphere");
+        private static PeerService Service = new PeerService();
+
+        static async Task Init()
+        {
+            var registered = await Interface.Loading("Registering to NetsphereServer", Service.RegisterRequest());
+
+            if(registered)
+            {
+                var catalog = await Interface.Loading("Loading catalog of files", Service.CatalogRequest());
+
+                if(!catalog.IsEmpty())
+                {
+                    var selected =  Interface.Menu(true, catalog.ToArray());
+                    var file = catalog.ElementAt(selected);
+
+                    var package = await Interface.Loading("Kindly asking for a file to another peer", Service.FileRequest(file));
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Sem itens");
+                }
+            }
+            else
+            {
+                Interface.DisplayMessage("Registration failed. NetsphereServer may be offline.", ConsoleColor.Red);
+            }
+        }
+
         static async Task Main(string[] args)
         {
-            var pService = new PeerService();
-            var catalog = await pService.CatalogRequest();
-            // _ = pService.RegisterRequest();
+            //int selectedClass = UIContext.Menu(true, "Warrior", "Bard", "Mage", "Archer", "Thief", "Assassin", "Cleric", "Paladin", "etc.");
+            //var service = new PeerService();
+            //await service.RegisterRequest();
 
-            foreach(var c in catalog)
-            {
-                Console.WriteLine(c.Name + " - " + c.Hash);
-            }
+
+            //service.CatalogRequest();
+            //var register = await pService.RegisterRequest();
+
+            await Init();
+            Console.ReadKey();
         }
     }
 }
