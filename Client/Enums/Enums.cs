e1ac457261d16b6417ef954149ed6e19896ea002
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-10-28 20:31:34 -0300

    Enums

---
+++ Client/Enums/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netsphere.Client.Enums
+{
+    public enum PacketType
+    {
+        FileRequest = 0x5C,
+        FileResponse
+    }
+}
