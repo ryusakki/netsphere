285ffe55ad039f35bea02997abdecaf5dd49539a
DouglasCarboni <douglas.dallavale@acad.pucrs.br>
2020-10-28 20:26:28 -0300

    Método de extensão IsEmpty

---
+++ Client/Extensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Netsphere.Client.Extensions
+{
+    public static class ListExtensions
+    {
+        public static bool IsEmpty<T>(this List<T> collection)
+        {
+            return collection.Count == 0;
+        }
+    }
+}
